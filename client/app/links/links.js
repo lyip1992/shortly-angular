019e4b6c38b9347ccdccab48a0c41301197ade37
lyip1992 <lyip1992@gmail.com>
2015-06-12 16:40:35 -0700

    implemented addLink and getLinks

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,22 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  $scope.data = {};
+
+  $scope.getLinks = function() {
+    Links.getLinks()
+      .then(function(data) {
+        $scope.data.links = data;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  $scope.init = function() {
+    $scope.getLinks();
+  };
+
+  $scope.init();
+
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,17 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+  $scope.link = {};
+
+  $scope.addLink = function() {
+    Links.addLink($scope.link)
+      .then(function(data) {
+        $scope.newLink = data;
+        console.log($scope.newLink.title);
+        console.log($scope.newLink.url);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 });
